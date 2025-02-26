9e34a24460e8f3b594b7040effa2c6be626b6a8a
DanielDavalos93 <dadaniel.1593@gmail.com>
2025-02-26 00:03:14 -0300

    arithmetic-evaluation

--- arithmetic.pl
+++ arithmetic.pl
@@ -1,17 +1,21 @@
-natural(zero).
-natural(succ(N)) :- natural(N).
+% Arithmetic expressions
+aexp(zero).
+aexp(succ(N)) :- aexp(N).
+aexp(add(A1,A2)) :- aexp(A1), aexp(A2).
+aexp(subt(A1,A2)) :- aexp(A1), aexp(A2).
+aexp(prod(A1,A2)) :- aexp(A1), aexp(A2).
 
 % Evaluation of numbers
-eval(zero,0,0).
-eval(succ(N),N1,N1) :- eval(N,N2,N2), N1 is N2+1.
+aeval(zero,0,0).
+aeval(succ(N),N1,N1) :- aeval(N,N2,N2), N1 is N2+1.
 
-% Evaluation of sums
-eval(add(A0,A1),S,N) :- eval(A0,S0,N0), eval(A1,S1,N1), 
+% Evaluation of arithmetic operations
+aeval(add(A0,A1),S,N) :- aeval(A0,S0,N0), aeval(A1,S1,N1), 
     N is N0+N1,S is S0+S1.
 
-eval(subt(A0,A1),S,N) :- eval(A0,S0,N0), eval(A1,S1,N1), 
+aeval(subt(A0,A1),S,N) :- aeval(A0,S0,N0), aeval(A1,S1,N1), 
     (N0 > N1 -> N is N0-N1; 
       N is 0), S is S0+S1.
 
-eval(prod(A0,A1),S,N) :- eval(A0,S0,N0), eval(A1,S1,N1),
+aeval(prod(A0,A1),S,N) :- aeval(A0,S0,N0), aeval(A1,S1,N1),
     N is N0*N1, S is S0+S1.
