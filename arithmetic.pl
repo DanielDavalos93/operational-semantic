713c3714fc8b388558dde3f8ca9c73937130e4ac
DanielDavalos93 <dadaniel.1593@gmail.com>
2025-03-02 01:44:14 -0300

    succ -> suc ; add pred as predecesor

--- arithmetic.pl
+++ arithmetic.pl
@@ -1,13 +1,14 @@
 % Arithmetic expressions
 aexp(zero).
-aexp(succ(N)) :- aexp(N).
+aexp(suc(N)) :- aexp(N).
+aexp(pred(N)) :- aexp(N).
 aexp(add(A1,A2)) :- aexp(A1), aexp(A2).
 aexp(subt(A1,A2)) :- aexp(A1), aexp(A2).
 aexp(prod(A1,A2)) :- aexp(A1), aexp(A2).
 
-% Evaluation of numbers
+% Evaluation of arithmetic expressions
 aeval(zero,0,0).
-aeval(succ(N),N1,N1) :- aeval(N,N2,N2), N1 is N2+1.
+aeval(suc(N),N1,N1) :- aeval(N,N2,N2), N1 is N2 + 1.
 
 % Evaluation of arithmetic operations
 aeval(add(A0,A1),S,N) :- aeval(A0,S0,N0), aeval(A1,S1,N1), 
