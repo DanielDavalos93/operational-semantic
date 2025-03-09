d1df446f2a3f6ae0584313c90d88f754a3493a4f
DanielDavalos93 <dadaniel.1593@gmail.com>
2025-03-09 02:00:42 -0300

    tests 2,3 - pairs

--- operational_semantics.pl
+++ operational_semantics.pl
@@ -1,4 +1,8 @@
 :- module(operational_semantics,
-  [aexp/1, aeval/3, bexp/1, beval/3, uno/1, dos/1, tres/1, cuatro/1, cinco/1, seis/1, siete/1, ocho/1, nueve/1, diez/1, lterm/1, len/2, fv/2, subst/3]).
+  [aexp/1, aeval/3, bexp/1, beval/3, uno/1, dos/1, tres/1, cuatro/1, cinco/1,
+  seis/1, siete/1, ocho/1, nueve/1, diez/1, 
+  lexp/1, len/2, fv/2, subst/3, tru/1, fls/1, ite/1, and/1, or/1,
+  value/1, beta_reduction/2, beta*/3,
+  test1/1, test2/1, test3/1]).
 :- ['arithmetic.pl','boolean.pl','test.pl', 'lambda.pl'].
 
--- test.pl
+++ test.pl
@@ -1,5 +1,5 @@
 % Terms 
-%:- dynamic succ/2.
+% Arithmetics
 uno(suc(zero)).
 dos(X) :- uno(Y), X = suc(Y).
 tres(X) :- dos(Y), X = suc(Y).
@@ -10,3 +10,34 @@ siete(X) :- seis(Y), X = suc(Y).
 ocho(X) :- siete(Y), X = suc(Y).
 nueve(X) :- ocho(Y), X = suc(Y).
 diez(X) :- nueve(Y), X = suc(Y).
+
+%%% lambda calculo %%%
+% Booleans
+tru(lam(t, lam(f, t))).
+fls(lam(t, lam(f, f))).
+ite(lam(l, lam(m, lam(n, app(app(l,m),n))))).
+and(lam(b,lam(m, app(app(b,m), lam(t, lam(f,f)))))).
+or(lam(b,lam(m, app(app(b, lam(t, lam(f,t))), m)))).
+
+% pairs
+pair(lam(f,lam(s,lam(b, app(b,app(f,s)))))).
+fst(lam(p,app(p,T))) :- tru(T).
+snd(lam(p,app(p,F))) :- fls(F).
+
+% Church numerals
+
+
+% tests
+% and tru fls -> fls
+test1(R) :- and(A), tru(T), fls(F), beta*(app(app(A, T), F), _, R),!. 
+% R = lam(t, lam(f,f)).
+
+% or tru fls -> tru
+test2(R) :- or(O), tru(T), fls(F), beta*(app(app(O,T), F), _, R),!.
+% R = lam(t, lam(f, t)).
+
+% fst (pair m n) -> m 
+test3(L,R) :- pair(P), fst(X), beta*(app(X, app(app(P, m), n)), L, R).
+
+
+
