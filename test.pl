4463017a7a205b2cf9861dee6d89b5feb50a3e09
DanielDavalos93 <dadaniel.1593@gmail.com>
2025-03-09 13:49:07 -0300

    church numeral

--- test.pl
+++ test.pl
@@ -20,24 +20,32 @@ and(lam(b,lam(m, app(app(b,m), lam(t, lam(f,f)))))).
 or(lam(b,lam(m, app(app(b, lam(t, lam(f,t))), m)))).
 
 % pairs
-pair(lam(f,lam(s,lam(b, app(b,app(f,s)))))).
+pair(lam(f,lam(s,lam(b, app(app(b,f),s))))).
 fst(lam(p,app(p,T))) :- tru(T).
 snd(lam(p,app(p,F))) :- fls(F).
 
 % Church numerals
+s(0,z).
+s(N, app(s, S)) :- s(N1, S), N is N1+1. 
+
+c(N, lam(s, lam(z, S))) :- s(N, S), !.
+
+% Arithmetic 
+%plus(lam(m,lam(n, lam(s, lam(z, app(m,app(s, app(app(n, s), z)))))))).
+%times(lam(m, lam(n, app(app(m, app(Plus, n), C0))))) :- plus(Plus), c(0,C0).
 
 
-% tests
-% and tru fls -> fls
-test1(R) :- and(A), tru(T), fls(F), beta*(app(app(A, T), F), _, R),!. 
+%%% tests %%%
+%1) and tru fls -> fls
+test1(R) :- and(A), tru(T), fls(F), beta(app(app(A, T), F), _, R),!. 
 % R = lam(t, lam(f,f)).
 
-% or tru fls -> tru
-test2(R) :- or(O), tru(T), fls(F), beta*(app(app(O,T), F), _, R),!.
+%2) or tru fls -> tru
+test2(R) :- or(O), tru(T), fls(F), beta(app(app(O,T), F), _, R),!.
 % R = lam(t, lam(f, t)).
 
-% fst (pair m n) -> m 
-test3(L,R) :- pair(P), fst(X), beta*(app(X, app(app(P, m), n)), L, R).
+%3) fst (pair m n) ->* m 
+test3(L,R) :- pair(P), fst(X), beta(app(X, app(app(P, m), n)), L, R).
 
-
-
+%4) (plus c2 c1) ->* c3
+%test4(R) :- plus(Plus), c(1,C1), c(2,C2), beta(app(app(Plus, C2),C1), _, R).
